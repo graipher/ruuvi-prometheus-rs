5a00bdceb75b38c7cd891b4ed8b74c6d1788cea2
Andreas Weiden <andreas.weiden@skillbyte.de>
2025-12-09 20:32:27 +0100

    (tests): add tests

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ mod bluetooth;
 mod config;
 mod metrics;
 mod ruuvi;
+#[cfg(test)]
+mod test_utils;
 use crate::bluetooth::{scan_and_listen, setup_adapter_monitor};
-use crate::config::config_from_env;
+use crate::config::Config;
 use crate::metrics::{Metrics, install_prometheus, spawn_process_collector};
 
-#[tokio::main(flavor = "current_thread")]
+#[tokio::main]
 async fn main() -> bluer::Result<()> {
-    let config = config_from_env();
+    let config = Config::from_env();
 
     install_prometheus(config.binding, config.idle_timeout);
     println!("Listening on {}", config.binding);
